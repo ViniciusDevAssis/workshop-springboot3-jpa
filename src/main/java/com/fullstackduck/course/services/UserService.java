0d9a1f58288c1d6626cad2f0e95efe44f19280ce
ViniciusDevAssis <viniciusfsa2014@gmail.com>
2023-02-23 22:57:24 -0300

    Service layer, component registration

---
+++ src/main/java/com/fullstackduck/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.fullstackduck.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fullstackduck.course.entities.User;
+import com.fullstackduck.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}

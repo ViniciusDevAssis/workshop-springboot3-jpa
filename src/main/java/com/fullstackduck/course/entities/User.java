f6b4c16fa72812dbaf236d3cbdb7ac6f1dde9967
ViniciusDevAssis <viniciusfsa2014@gmail.com>
2023-02-23 23:18:14 -0300

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/com/fullstackduck/course/entities/User.java
+++ src/main/java/com/fullstackduck/course/entities/User.java
@@ -1,12 +1,15 @@
 package com.fullstackduck.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +24,9 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 		
@@ -74,6 +80,10 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 	@Override
 	public int hashCode() {
@@ -91,6 +101,5 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
+
 }

f3a819f8993af08fad7418362b3c9c739ba3a5ce
ViniciusDevAssis <viniciusfsa2014@gmail.com>
2023-02-25 01:23:14 -0300

    Exception handling - delete

--- src/main/java/com/fullstackduck/course/services/UserService.java
+++ src/main/java/com/fullstackduck/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.fullstackduck.course.entities.User;
 import com.fullstackduck.course.repositories.UserRepository;
+import com.fullstackduck.course.services.exceptions.DatabaseException;
 import com.fullstackduck.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(long id, User obj) {
